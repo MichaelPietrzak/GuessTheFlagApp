bb86d5df2afa1b70bbd84d4c0a2ff878b4e7b231
Michal Pietrzak <michal.pietrzak18@gmail.com>
2020-10-14 23:31:14 +0200

    project setup

---
+++ GuessTheFlagApp/GuessTheFlagApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GuessTheFlagApp
+//
+//  Created by Michal Pietrzak on 14/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
