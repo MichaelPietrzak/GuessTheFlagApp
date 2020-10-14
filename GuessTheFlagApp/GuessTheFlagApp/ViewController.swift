0384796d511862d1646680470fcbfb02c8dfb238
Michal Pietrzak <michal.pietrzak18@gmail.com>
2020-10-14 23:38:50 +0200

    created array of countries + function with shuffled flags + randomized correct answer

--- GuessTheFlagApp/GuessTheFlagApp/ViewController.swift
+++ GuessTheFlagApp/GuessTheFlagApp/ViewController.swift
@@ -12,10 +12,32 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
     }
 
+    func askQuestion() {
+        
+        countries.shuffle()
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        print("\(countries[0].uppercased()), \(countries[1].uppercased()), \(countries[2].uppercased())")
+        
+        correctAnswer = Int.random(in: 0...2)
+        print("\(correctAnswer) : \(countries[correctAnswer].uppercased())")
+        
+        title = countries[correctAnswer].uppercased()
+    }
 
 }
 
